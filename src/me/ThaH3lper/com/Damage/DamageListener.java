327b72ef760e27733fcacbc9c1d7669f7b04428a
Richard Harrah <topplethenun@hawksharbor.info>
2013-01-24 20:27:53 -0500

    cleaning warnings and removing unnecessary parameters

--- src/me/ThaH3lper/com/Damage/DamageListener.java
+++ src/me/ThaH3lper/com/Damage/DamageListener.java
@@ -23,138 +23,161 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
-public class DamageListener implements Listener{
-	private EpicBoss eb;
-	public DamageListener(EpicBoss boss)
-	{
-		eb = boss;
-	}
-	
-	@EventHandler(priority=EventPriority.HIGH)
-	  public void Damage(EntityDamageByEntityEvent e)
-	{
-		Entity Damager = e.getDamager();
-		Entity Hited = e.getEntity();
-		//Make Damager to a Living entity insted of Arrow, Fireball etc
-		//Also cheack if Arrow is not from dispenser;
-		if(Damager instanceof Arrow)
-		{
-			Arrow a = (Arrow) Damager;
-			Damager = a.getShooter();
-			if(Damager == null && eb.bossCalculator.isBoss(Hited))
-			{
-				e.setCancelled(true);
-				return;
-			}
-			else if(Damager == null)
-			{
-				return;
-			}
-		}
-		if(Damager instanceof Fireball)
-		{
-			Fireball a = (Fireball) Damager;
-			Damager = a.getShooter();
-		}
-		if(Damager instanceof SmallFireball)
-		{
-			SmallFireball a = (SmallFireball) Damager;
-			Damager = a.getShooter();
-		}
-		if(Damager instanceof Snowball)
-		{
-			Snowball a = (Snowball) Damager;
-			Damager = a.getShooter();
-		}
-		if(Damager instanceof Egg)
-		{
-			Egg a = (Egg) Damager;
-			Damager = a.getShooter();
-		}
-		if(Damager instanceof ThrownPotion)
-		{
-			ThrownPotion a = (ThrownPotion) Damager;
-			Damager = a.getShooter();
-		}
-		if(Damager instanceof LivingEntity && Hited instanceof LivingEntity)
-		{
-			int damage = e.getDamage();
-			LivingEntity hited = (LivingEntity) Hited; //Hited entity is now "hited" since it's a LivingEntity
-			if(eb.bossCalculator.isBoss(Hited))
-			{
-				e.setDamage(1);
-				if(!eb.bossCalculator.BossHited(hited))
-				{
-					Boss boss = eb.bossCalculator.getBoss(Hited);
-					boss.sethealth(boss.getHealth() - damage);
-					hited.setHealth(hited.getMaxHealth());
-					if(Damager instanceof Player)
-					{
-						eb.skillhandler.skills(boss, (Player) Damager);
-					}
-					if(boss.getHealth() <= 0)
-					{
-						if(Damager instanceof Player)
-						{
-							BossDeathEvent event = new BossDeathEvent(eb, (Player) Damager, boss);
-							Bukkit.getServer().getPluginManager().callEvent(event);
-						}
-						eb.damagemethods.deathBoss(boss);
-					}
-					else if(boss.getShowHp())
-					{
-						if(Damager instanceof Player)
-						{
-							Player p = (Player) Damager;
-							if(eb.percentage == false)
-							{
-									String s = eb.name + ChatColor.RED + boss.getName() + ChatColor.GRAY + " [" +ChatColor.DARK_RED + boss.getHealth() + ChatColor.GRAY + "/" + ChatColor.DARK_RED + boss.getMaxHealth() + ChatColor.GRAY + "]";
-									p.sendMessage(s);							
-							}
-							else
-							{
-								double per = (((double)boss.getHealth()/boss.getMaxHealth())*10)+1;
-								if(!boss.hasPercent((int)per))
-								{
-									String s = eb.name + ChatColor.RED + boss.getName() + ChatColor.GRAY + " [" +ChatColor.DARK_RED + ((int)per*10) + "%" + ChatColor.GRAY + "]";
-									for(Player player : eb.skillhandler.getPlayersRadious(20, boss))
-									{
-										player.sendMessage(s);
-									}
-									boss.addPercent((int)per);
-								}
-							}
-						}
-					}
-				}
-			}
-			else if(eb.bossCalculator.isBoss(Damager))
-			{
-				Boss boss = eb.bossCalculator.getBoss(Damager);
-				e.setDamage(boss.getDamage());
-			}
-		}
-	}
-	@EventHandler(priority=EventPriority.HIGH)
-	  public void BossNoLose(EntityDamageEvent event)
-	{
-		if(event.getEntity() != null)
-		{
-			if(eb.bossCalculator.isBoss(event.getEntity())){
-				if(!(event.getCause() == DamageCause.ENTITY_ATTACK || event.getCause() == DamageCause.PROJECTILE ||  event.getCause() == DamageCause.MAGIC))
-				{
-					event.setCancelled(true);
-				}
-			}
-		}
-	}
-	@EventHandler(priority=EventPriority.HIGH)
-	  public void BossNoFire(EntityCombustEvent e)
-	{	
-		if(eb.bossCalculator.isBoss(e.getEntity()))
-		{
-			e.setCancelled(true);
-		}
-	}
-	
+public class DamageListener implements Listener
+{
+    private final EpicBoss eb;
+
+    public DamageListener(EpicBoss boss)
+    {
+        eb = boss;
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void BossNoFire(EntityCombustEvent e)
+    {
+        if (eb.bossCalculator.isBoss(e.getEntity()))
+        {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void BossNoLose(EntityDamageEvent event)
+    {
+        if (event.getEntity() != null)
+        {
+            if (eb.bossCalculator.isBoss(event.getEntity()))
+            {
+                if (!(event.getCause() == DamageCause.ENTITY_ATTACK
+                        || event.getCause() == DamageCause.PROJECTILE || event
+                            .getCause() == DamageCause.MAGIC))
+                {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void Damage(EntityDamageByEntityEvent e)
+    {
+        Entity Damager = e.getDamager();
+        Entity Hited = e.getEntity();
+        // Make Damager to a Living entity insted of Arrow, Fireball etc
+        // Also cheack if Arrow is not from dispenser;
+        if (Damager instanceof Arrow)
+        {
+            Arrow a = (Arrow) Damager;
+            Damager = a.getShooter();
+            if (Damager == null && eb.bossCalculator.isBoss(Hited))
+            {
+                e.setCancelled(true);
+                return;
+            }
+            else if (Damager == null)
+            {
+                return;
+            }
+        }
+        if (Damager instanceof Fireball)
+        {
+            Fireball a = (Fireball) Damager;
+            Damager = a.getShooter();
+        }
+        if (Damager instanceof SmallFireball)
+        {
+            SmallFireball a = (SmallFireball) Damager;
+            Damager = a.getShooter();
+        }
+        if (Damager instanceof Snowball)
+        {
+            Snowball a = (Snowball) Damager;
+            Damager = a.getShooter();
+        }
+        if (Damager instanceof Egg)
+        {
+            Egg a = (Egg) Damager;
+            Damager = a.getShooter();
+        }
+        if (Damager instanceof ThrownPotion)
+        {
+            ThrownPotion a = (ThrownPotion) Damager;
+            Damager = a.getShooter();
+        }
+        if (Damager instanceof LivingEntity && Hited instanceof LivingEntity)
+        {
+            int damage = e.getDamage();
+            LivingEntity hited = (LivingEntity) Hited; // Hited entity is now
+                                                       // "hited" since it's a
+                                                       // LivingEntity
+            if (eb.bossCalculator.isBoss(Hited))
+            {
+                e.setDamage(1);
+                if (!eb.bossCalculator.BossHited(hited))
+                {
+                    Boss boss = eb.bossCalculator.getBoss(Hited);
+                    boss.sethealth(boss.getHealth() - damage);
+                    hited.setHealth(hited.getMaxHealth());
+                    if (Damager instanceof Player)
+                    {
+                        eb.skillhandler.skills(boss, (Player) Damager);
+                    }
+                    if (boss.getHealth() <= 0)
+                    {
+                        if (Damager instanceof Player)
+                        {
+                            BossDeathEvent event = new BossDeathEvent(
+                                    (Player) Damager, boss);
+                            Bukkit.getServer().getPluginManager()
+                                    .callEvent(event);
+                        }
+                        eb.damagemethods.deathBoss(boss);
+                    }
+                    else if (boss.getShowHp())
+                    {
+                        if (Damager instanceof Player)
+                        {
+                            Player p = (Player) Damager;
+                            if (eb.percentage == false)
+                            {
+                                String s = eb.name + ChatColor.RED
+                                        + boss.getName() + ChatColor.GRAY
+                                        + " [" + ChatColor.DARK_RED
+                                        + boss.getHealth() + ChatColor.GRAY
+                                        + "/" + ChatColor.DARK_RED
+                                        + boss.getMaxHealth() + ChatColor.GRAY
+                                        + "]";
+                                p.sendMessage(s);
+                            }
+                            else
+                            {
+                                double per = (((double) boss.getHealth() / boss
+                                        .getMaxHealth()) * 10) + 1;
+                                if (!boss.hasPercent((int) per))
+                                {
+                                    String s = eb.name + ChatColor.RED
+                                            + boss.getName() + ChatColor.GRAY
+                                            + " [" + ChatColor.DARK_RED
+                                            + ((int) per * 10) + "%"
+                                            + ChatColor.GRAY + "]";
+                                    for (Player player : eb.skillhandler
+                                            .getPlayersRadious(20, boss))
+                                    {
+                                        player.sendMessage(s);
+                                    }
+                                    boss.addPercent((int) per);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if (eb.bossCalculator.isBoss(Damager))
+            {
+                Boss boss = eb.bossCalculator.getBoss(Damager);
+                e.setDamage(boss.getDamage());
+            }
+        }
+    }
+
 }
--- src/me/ThaH3lper/com/Skills/AllSkills/Command.java
+++ src/me/ThaH3lper/com/Skills/AllSkills/Command.java
@@ -8,56 +8,64 @@ import me.ThaH3lper.com.Boss.Boss;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-public class Command {
-	private EpicBoss eb;
-	Random r = new Random();
-	
-	//- command &2I_Will_Kill_You! <500 0.2
-	
-	public Command(EpicBoss boss)
-	{
-		eb = boss;
-	}
-	public void executeCommand(String s, Boss b, int index, Player p)
-	{
-		String[] parts = s.split(" ");
-		float chance = Float.parseFloat(parts[3]);
-		if(parts[2].contains(">"))
-		{
-			String exe = parts[2].replace(">", "");
-			if(b.getHealth() > Integer.parseInt(exe))
-			{
-				sendCommand(parts[1], chance, p, b);
-			}
-		}
-		else if(parts[2].contains("="))
-		{
-			String exe = parts[2].replace("=", "");
-			if(b.getHealth() <= Integer.parseInt(exe))
-			{
-				sendCommand(parts[1], chance, p, b);
-				b.setRemoveSkill(index);
-			}
-		}
-		else if(parts[2].contains("<"))
-		{
-			String exe = parts[2].replace("<", "");
-			if(b.getHealth() < Integer.parseInt(exe))
-			{
-				sendCommand(parts[1], chance, p, b);
-			}
-		}
-	}
-	public void sendCommand(String s, float chance, Player p, Boss b)
-	{
-		if(r.nextFloat() <= chance)
-		{
-			
-			s = s.replace("_", " ");
-			s = s.replace("$player", p.getName());
-			s = s.replace("$boss", b.getName());
-			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), s);
-		}
-	}
+public class Command
+{
+    private final EpicBoss eb;
+    Random r = new Random();
+
+    // - command &2I_Will_Kill_You! <500 0.2
+
+    public Command(EpicBoss boss)
+    {
+        this.eb = boss;
+    }
+
+    public void executeCommand(String s, Boss b, int index, Player p)
+    {
+        String[] parts = s.split(" ");
+        float chance = Float.parseFloat(parts[3]);
+        if (parts[2].contains(">"))
+        {
+            String exe = parts[2].replace(">", "");
+            if (b.getHealth() > Integer.parseInt(exe))
+            {
+                sendCommand(parts[1], chance, p, b);
+            }
+        }
+        else if (parts[2].contains("="))
+        {
+            String exe = parts[2].replace("=", "");
+            if (b.getHealth() <= Integer.parseInt(exe))
+            {
+                sendCommand(parts[1], chance, p, b);
+                b.setRemoveSkill(index);
+            }
+        }
+        else if (parts[2].contains("<"))
+        {
+            String exe = parts[2].replace("<", "");
+            if (b.getHealth() < Integer.parseInt(exe))
+            {
+                sendCommand(parts[1], chance, p, b);
+            }
+        }
+    }
+
+    public EpicBoss getEpicBoss()
+    {
+        return eb;
+    }
+
+    public void sendCommand(String s, float chance, Player p, Boss b)
+    {
+        if (r.nextFloat() <= chance)
+        {
+
+            s = s.replace("_", " ");
+            s = s.replace("$player", p.getName());
+            s = s.replace("$boss", b.getName());
+            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), s);
+        }
+    }
 
 }
